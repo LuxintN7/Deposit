2b53a9bd388aacbd69cbb512ce777a7f6c005c9e
Anastasia Nevidomska <nevidomska.a@gmail.com>
2015-08-25 00:18:34 +0300

    delete 3 methods from the IInterestPaymentHandler interface and add the AddInterest() method

--- DomainLogic/Handlers/IInterestPaymentHandler.cs
+++ DomainLogic/Handlers/IInterestPaymentHandler.cs
@@ -10,8 +10,6 @@ namespace DomainLogic.Handlers
         void ExtendDeposit(int depositId, DateTime today);
         void SetLastInterestPaymentDate(int depositId, DateTime date);
         void SetDepositState(int depositId, string stateName);
-        void IncreaseDepositBalanceByInterestAmount(int depositId, decimal interestAmount);
-        void IncreaseCardBalanceByInterestAmount(string cardId, decimal interestAmount);
-        void AddCardHistoryRecord(string cardId, string cardHistoryDescription);
+        void AddInterest(int depositId);
     }
 }
