494c2d6a1a3a668801bb3b50115b74b757162dc7
Anastasia Nevidomska <nevidomska.a@gmail.com>
2015-08-24 23:20:41 +0300

    create the IInterestPaymentHandler interface

---
+++ DomainLogic/Handlers/IInterestPaymentHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DomainLogic.Model;
+
+namespace DomainLogic.Handlers
+{
+    public interface IInterestPaymentHandler
+    {
+        Deposits GetActiveDeposits();
+        void ExtendDeposit(int depositId);
+        void AddInterest(int depositId);
+        void SetLastInterestPaymentDate(int depositId, DateTime date);
+        void SetDepositState(int depositId, string stateName);
+    }
+}
