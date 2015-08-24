dc9fa8a2c82f0fdb318bf60d9cc8b0ad16c89d82
Anastasia Nevidomska <nevidomska.a@gmail.com>
2015-08-25 00:10:12 +0300

    add 3 methods to the IInterestPaymentHandler interface

--- DomainLogic/Handlers/IInterestPaymentHandler.cs
+++ DomainLogic/Handlers/IInterestPaymentHandler.cs
@@ -10,5 +10,8 @@ namespace DomainLogic.Handlers
         void ExtendDeposit(int depositId, DateTime today);
         void SetLastInterestPaymentDate(int depositId, DateTime date);
         void SetDepositState(int depositId, string stateName);
+        void IncreaseDepositBalanceByInterestAmount(int depositId, decimal interestAmount);
+        void IncreaseCardBalanceByInterestAmount(string cardId, decimal interestAmount);
+        void AddCardHistoryRecord(string cardId, string cardHistoryDescription);
     }
 }
