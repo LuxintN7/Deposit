6aaba1c8b23bf1fa1f171bd87305d973b0a0aa05
Anastasia Nevidomska <nevidomska.a@gmail.com>
2015-08-28 20:58:53 +0300

    create DepositStatesExtension

---
+++ DepositDatabase/DepositStatesExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using DepositDatabase.Model;
+
+namespace DepositDatabase
+{
+    public static class DepositStatesExtension
+    {
+        public static DomainLogic.Model.DepositStates ToDomainLogic(this DepositStates state)
+        {
+            return new DomainLogic.Model.DepositStates()
+            { 
+               Id = state.Id,
+               Name = state.Name
+            };
+        }
+
+        public static List<DomainLogic.Model.DepositStates> ToDomainLogic(this List<DepositStates> list)
+        {
+            var states = new List<DomainLogic.Model.DepositStates>();
+
+            foreach (var state in list)
+            {
+                states.Add(state.ToDomainLogic());
+            }
+
+            return states;
+        }
+    }
+}
