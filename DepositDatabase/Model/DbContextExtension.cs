a334d6268ce0e45c0cf97da5aca5f37854bf0fa3
Anastasia Nevidomska <nevidomska.a@gmail.com>
2015-09-13 21:34:11 +0300

    implement the GetInstance() method for DepositEntities

--- DepositDatabase/Model/DbContextExtension.cs
+++ DepositDatabase/Model/DbContextExtension.cs
@@ -4,16 +4,36 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using DepositDatabase.Model;
 
 namespace DepositDatabase.Model
 {
-    public static class DbContextExtension
+    public static class DepositEntitiesExtension
     {
-        public static bool HasUnsavedChanges(this DbContext dbContext)
+        public static bool HasUnsavedChanges(this DepositEntities dbContext)
         {
             return dbContext.ChangeTracker.Entries().Any(e => e.State == EntityState.Added
                                            || e.State == EntityState.Modified
                                            || e.State == EntityState.Deleted);
+        }
+        
+        public static DepositEntities GetInstance()
+        {
+            var instance = (DepositEntities) HttpContext.Current.Items[typeof (DepositEntities)];
+            if (instance == null)
+            {
+                instance = new DepositEntities();
+                HttpContext.Current.Items[typeof (DepositEntities)] = instance;
+                HttpContext.Current.AddOnRequestCompleted(OnRequestCompleted);
+            }
+            return instance;
+        }
+
+        private static void OnRequestCompleted(HttpContext context)
+        {
+            var instance = (DepositEntities)HttpContext.Current.Items[typeof(DepositEntities)];
+            if (instance != null) instance.Dispose();
         }
     }
 }
